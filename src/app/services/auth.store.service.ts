d256bd8f9ae920efdd8b58f99f6a34c01fe0afc4
MahmoudsMohammed <mahmoudsmohammed24@gmail.com>
2024-12-10 10:50:49 +0200

    setup the auth store service

---
+++ src/app/services/auth.store.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from "@angular/core";
+import { BehaviorSubject, Observable } from "rxjs";
+import { User } from "../model/user";
+import { HttpClient } from "@angular/common/http";
+import { map, shareReplay, tap } from "rxjs/operators";
+
+@Injectable({ providedIn: "root" })
+export class authStoreService {
+  private user = new BehaviorSubject<User>(null);
+  user$ = this.user.asObservable();
+  loginIn$: Observable<boolean>;
+  LogOut$: Observable<boolean>;
+
+  constructor(private http: HttpClient) {
+    // check for user data in LS
+    if (localStorage.getItem("user")) {
+      this.user.next(JSON.parse(localStorage.getItem("user")));
+    }
+    // Map user Data into a boolean expression
+    this.loginIn$ = this.user$.pipe(map((user) => !!user));
+    this.LogOut$ = this.loginIn$.pipe(map((data) => !data));
+  }
+
+  login(email: string, pass: string): Observable<User> {
+    // this is http request make it hot as best practice to reduce requests
+    return this.http.post<User>("/api/login", { email, pass }).pipe(
+      tap((user) => {
+        this.user.next(user);
+        localStorage.setItem("user", JSON.stringify(user));
+      }),
+      shareReplay(1)
+    );
+  }
+
+  logOut() {
+    this.user.next(null);
+    localStorage.removeItem("user");
+  }
+}
